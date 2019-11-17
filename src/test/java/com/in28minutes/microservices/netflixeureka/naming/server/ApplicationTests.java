42d7447a74f6a02857c7a9cb785013ea4d7b7318
devendra2690 <devendra_patil26@yahoo.com>
2019-11-18 00:50:19 +0530

    Adding Zuul dependency

---
+++ src/test/java/com/in28minutes/microservices/netflixeureka/naming/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.netflixeureka.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
